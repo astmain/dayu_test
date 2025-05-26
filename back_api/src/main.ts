a8c186e225d16200bf5c046f2fb3e75d37eb4572
xupeng <1311192345@qq.com>
2025-05-26 17:18:08 +0800

    111

--- back_api/src/main.ts
+++ back_api/src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 import { createSwagger } from './core/swagger';
 import { GLOBAL_VALIDATION_PIPE } from './processor/pipe/globalValidationPipe';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER as 'NestWinston')); // 使用winston替换掉nest内置日志
+  console.log(`111---process.env.PORT:`, process.env.PORT);
+  console.log(`http://127.0.0.1:8001/api-docs                admin/123456`);
+
   // app.use(
   //   helmet({
   //     crossOriginEmbedderPolicy: false,
@@ -21,11 +25,12 @@ async function bootstrap() {
   //     crossOriginResourcePolicy: { policy: 'cross-origin' }, // 加这一行才能加载图片资源
   //   }),
   // );
-  createSwagger(app);
 
   app.setGlobalPrefix('api');
+  createSwagger(app);
 
   app.useGlobalPipes(GLOBAL_VALIDATION_PIPE); // 全局类转换校验  定义了dto的会自动转换
   await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
